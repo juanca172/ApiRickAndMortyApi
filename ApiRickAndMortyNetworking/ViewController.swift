b3e922727b811dd002f0bb27b14cb60c2ee90972
juanca172 <juancamilofonseca603@gmail.com>
2022-06-28 19:58:17 -0500

    Initial Commit

---
+++ ApiRickAndMortyNetworking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiRickAndMortyNetworking
+//
+//  Created by Juan Camilo Fonseca Gomez on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
